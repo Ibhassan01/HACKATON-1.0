83039639955e98d8febd1777ef7766659ebd3ede
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-09-19 19:07:02 +0100

    Nav-bar completed

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let body = document.body;
 let menuIconEl =document.getElementById('menu-icon');
 let topMenuEl = document.getElementById('nav')
+let closeMenuEl = document.getElementById('close')
 
 menuIconEl.addEventListener('click', () => {
     topMenuEl.style.top = '0'
 })
+closeMenuEl.addEventListener('click', () => {
+    topMenuEl.style.top = '-100%'
+});

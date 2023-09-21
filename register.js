ce91402c9b4283b196c02d0ab6e1f0319a4dcbc0
Ibhassan01 <olorunnisolaibrahim46@gmail.com>
2023-09-21 14:17:32 +0100

    completed register page

---
+++ register.js
@@ -0,0 +1,14 @@
+// const select = document.getElementById("categories,group");
+        
+// select.addEventListener("change", function () {
+//     const selectedOption = this.options[this.selectedIndex];
+//     if (selectedOption.value === "") {
+//         selectedOption.disabled = true;
+//     }
+// });
+
+
+
+
+
+
